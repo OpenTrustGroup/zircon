1182dc1c2729f7960752c202bc445b8207766dc4
James Kung <kong1191@gmail.com>
2018-04-17 22:12:12 +0800

    [kernel][smc] Implement non-secure share memory machanism
    Non-secure share memory is for exchanging data between REE and TEE.
    TEE reads share memory configuration from mdi at boot time. REE
    can issue a SMC_FC_GET_SHM_CONFIG fastcall to TEE to retrive the
    share memory config.
    
    Change-Id: I1a7789a9aa96f4854812a1148be70a1016e5e16d

--- third_party/lib/sm/include/lib/sm.h
+++ third_party/lib/sm/include/lib/sm.h
@@ -33,37 +33,46 @@ typedef uint64_t ns_addr_t;
 typedef uint32_t ns_size_t;
 
 typedef struct ns_page_info {
-	uint64_t attr;
+    uint64_t attr;
 } ns_page_info_t;
 
 typedef long (*smc32_handler_t)(smc32_args_t *args);
 
 typedef struct smc32_entity {
-	smc32_handler_t fastcall_handler;
-	smc32_handler_t nopcall_handler;
-	smc32_handler_t stdcall_handler;
+    smc32_handler_t fastcall_handler;
+    smc32_handler_t nopcall_handler;
+    smc32_handler_t stdcall_handler;
 } smc32_entity_t;
+
+typedef struct ns_shm_info {
+    uint32_t pa;
+    uint32_t size;
+    bool use_cache;
+} ns_shm_info_t;
 
 __BEGIN_CDECLS
 
 /* Schedule Secure OS */
-long sm_sched_secure(smc32_args_t *args);
+long sm_sched_secure(smc32_args_t* args);
 
 /* Schedule Non-secure OS */
-void sm_sched_nonsecure(long retval, smc32_args_t *args);
+void sm_sched_nonsecure(long retval, smc32_args_t* args);
 
 /* Handle an interrupt */
 void sm_handle_irq(void);
 
+/* Get Non-secure share memory configuration */
+void sm_get_shm_config(ns_shm_info_t* shm);
+
 /* Version */
-long smc_sm_api_version(smc32_args_t *args);
+long smc_sm_api_version(smc32_args_t* args);
 
 /* Interrupt controller irq/fiq support */
-long smc_intc_get_next_irq(smc32_args_t *args);
+long smc_intc_get_next_irq(smc32_args_t* args);
 
 /* Private functions */
-long smc_fastcall_secure_monitor(smc32_args_t *args);
-long smc_undefined(smc32_args_t *args);
+long smc_fastcall_secure_monitor(smc32_args_t* args);
+long smc_undefined(smc32_args_t* args);
 
 __END_CDECLS
 
