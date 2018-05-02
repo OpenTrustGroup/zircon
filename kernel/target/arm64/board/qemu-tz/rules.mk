67beaa81f3765b5009b16c01c3ffa91017ee9684
James Kung <kong1191@gmail.com>
2018-05-02 21:14:45 +0800

    [kernel][qemu-tz] zircon upgrade and support new bootdata struct
    Change-Id: I6ab8a8f170fc598b81aab9efdc084730e4b8bab9

---
+++ kernel/target/arm64/board/qemu-tz/rules.mk
@@ -0,0 +1,26 @@
+# Copyright 2018 Open Trust Group
+# Copyright 2016 The Fuchsia Authors
+# Copyright (c) 2008-2015 Travis Geiselbrecht
+#
+# Use of this source code is governed by a MIT-style
+# license that can be found in the LICENSE file or at
+# https://opensource.org/licenses/MIT
+
+LOCAL_DIR := $(GET_LOCAL_DIR)
+
+PLATFORM_BOARD_NAME := qemu-tz
+PLATFORM_USE_SHIM := true
+
+include make/board.mk
+
+# qemu needs a shimmed kernel
+QEMU_TZ_ZIRCON := $(BUILDDIR)/qemu-tz-zircon.bin
+QEMU_TZ_BOOT_SHIM := $(BUILDDIR)/qemu-tz-boot-shim.bin
+
+# prepend shim to kernel image
+$(QEMU_TZ_ZIRCON): $(MKBOOTFS) $(QEMU_TZ_BOOT_SHIM) $(OUTLKBIN)
+	$(call BUILDECHO,generating $@)
+	$(NOECHO)$(MKBOOTFS) -o $@ $(OUTLKBIN) --header $(QEMU_TZ_BOOT_SHIM) --header-align $(KERNEL_ALIGN)
+
+GENERATED += $(QEMU_TZ_ZIRCON)
+EXTRA_BUILDDEPS += $(QEMU_TZ_ZIRCON)
