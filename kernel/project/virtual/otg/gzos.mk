2ab38f1fee427a32adc5c709a7582ed8abbcfdaf
James Kung <kong1191@gmail.com>
2018-08-06 15:51:20 +0800

    [gzos] Update copyright
    Change-Id: I08a23af0ad6bea26a509f32646042edac735f78f

--- kernel/project/virtual/otg/gzos.mk
+++ kernel/project/virtual/otg/gzos.mk
@@ -1,5 +1,4 @@
-# Copyright 2016 The Fuchsia Authors
-# Copyright (c) 2008-2015 Travis Geiselbrecht
+# Copyright 2018 Open Trust Group
 #
 # Use of this source code is governed by a MIT-style
 # license that can be found in the LICENSE file or at
--- system/public/zircon/syscalls/smc_service.h
+++ system/public/zircon/syscalls/smc_service.h
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2013-2016 Google Inc. All rights reserved
- * Copyright (c) Open Trust Group
+ * Copyright (c) 2018 Open Trust Group
  *
  * Permission is hereby granted, free of charge, to any person obtaining
  * a copy of this software and associated documentation files
